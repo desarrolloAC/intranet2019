f8fb8cf9d32befc9ea2c086ed52e30dfef7231d7
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2018-12-26 16:42:15 -0400

    la conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -7,7 +7,7 @@ function conectar()
 {
 
 	//REALIZO LA CONEXION A MYSQL
-	if(!($link = mysql_connect("192.168.30.25","workbench","12345678")))
+	if(!($link = mysql_connect("192.168.30.90","workbench","12345678")))
 	{
 	   	echo"Error conectando a la base de datos.";
 	 	exit();

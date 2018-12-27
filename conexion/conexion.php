943927e74f3fa1b0b05ba1327d4064b24b0aa472
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2018-12-27 12:07:37 -0400

    cambios del 27-12-2018

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -3,18 +3,16 @@
 //PARA NO MOSTRAR LOS ERRORES
 //error_reporting(0);
 
-function conectar()
-{
+function conectar() {
 
 	//REALIZO LA CONEXION A MYSQL
-	if(!($link = mysql_connect("192.168.30.90","workbench","12345678")))
-	{
+	if (!($link = mysql_connect("192.168.30.90","workbench","12345678"))) {
 	   	echo"Error conectando a la base de datos.";
 	 	exit();
 	}
 
 	//SELECCIONO LA BASE DE DATOS QUE VOY A USAR
-	if(!mysql_select_db("intranet", $link)){
+	if (!mysql_select_db("intranet", $link)) {
 		echo"Error seleccionando la Base de Datos.";
 		exit();
 	 }

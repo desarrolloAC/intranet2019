85fc27a1cfffbea663989b648f39517405999d21
Ana Tapia <chestkary@gmail.com>
2019-01-16 11:18:20 -0400

    cambios 16-01-2019

--- formularioPublicaciones/avanceInformativo.php
+++ formularioPublicaciones/avanceInformativo.php
@@ -25,6 +25,51 @@ $org = mysqli_fetch_array($nombreOrg, MYSQLI_ASSOC);
         }
 
     }
+    
+    function getFileSize (fileName) {
+        if (document.layers) {
+          if (navigator.javaEnabled()) {
+            var file = new java.io.File(fileName);
+            if (location.protocol.toLowerCase() !== 'file:')
+              netscape.security.PrivilegeManager.enablePrivilege(
+              'UniversalFileRead'
+              );
+            return file.length();
+          }
+          else return -1;
+        }
+        else if (document.all) {
+          window.oldOnError = window.onerror;
+          window.onerror = function (err) {
+            if (err.indexOf('Automation') !== -1) {
+              alert('file access not possible');
+              return true;
+            }
+            else
+              return false;
+          };
+          var fso = new ActiveXObject('Scripting.FileSystemObject');
+          var file = fso.GetFile(fileName);
+          window.onerror = window.oldOnError;
+          return file.Size;
+        }
+    }
+    
+    function remove(){
+        
+        var n=frm.elements.length
+        var temp=new Array(n);
+        for(i=0;i<=n-1;i++)
+        {
+         temp[i]=frm.elements[i].value
+        }
+        document.frm.reset()
+        for(i=0;i<=n-1;i++)
+        {
+         frm.elements[i].value=temp[i]
+        }
+
+    }
 
 </script>
 
@@ -54,7 +99,7 @@ $org = mysqli_fetch_array($nombreOrg, MYSQLI_ASSOC);
 
             <label id="tituloCaracteresAvanceInformativo">Caracteres Restantes</label>
 
-            <input id="btnImagenAvanceInformativo" type="file" name="archivo" required>
+            <input id="btnImagenAvanceInformativo" type="file" name="archivo" onchange="if((getFileSize(this.form.fileName.value))>300000){remove();alert('el fichero supera los 300 KB ')}" required>
 
             <img id="imgSalida" width="26%" height="21%" src="" />
 

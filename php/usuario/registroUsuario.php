e1bbf3a5270162d4f624045d2692da61c60502eb
Ana Tapia <chestkary@gmail.com>
2019-02-20 16:19:18 -0400

    cambios 20-02-2019

--- php/usuario/registroUsuario.php
+++ php/usuario/registroUsuario.php
@@ -75,7 +75,7 @@ try {
             $destino = $_SERVER['DOCUMENT_ROOT'] . '/intranet/' . $destino_temp;
 
             //mover la foto
-            if (!move_uploaded_file($origen, $destino)) {
+            if (move_uploaded_file($origen, $destino)) {
                 throw new RuntimeException('No se pudo mover el archivo '.$_FILES['btnImagen']['name'].'.');
             }
 
